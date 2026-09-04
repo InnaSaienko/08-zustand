96891eaf896b135710cfd4d393ea2664a6d1ff9c
Inna Saienko <inna.saienko.nl@gmail.com>
2026-09-04 09:40:21 +0200

    feat(seo): add Open Graph and Twitter metadata for social sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,31 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
+const vercelLink = process.env.NEXT_PUBLIC_WEBSITE_VERCEL_URL ?? "https://08-zustand-eight-sage.vercel.app";
+
 export const metadata: Metadata = {
+    metadataBase: new URL(vercelLink),
     title: "Note HUB",
     description: "The NotesClient for your notes",
+    openGraph: {
+        type: "website",
+        title: "Note HUB",
+        description: "",
+        url: "/",
+        siteName: "NoteHub",
+        images: [{
+            url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+            width: "100%",
+            height: "auto",
+            alt: "NoteHub",
+        }]
+    },
+    twitter: {
+        card: "summary",
+        title: "Note HUB",
+        description: "",
+        images: ["https://ac.goit.global/fullstack/react/notehub-og-meta.jpg"],
+    }
 };
 
 export default function RootLayout({children, modal}: Readonly<{
